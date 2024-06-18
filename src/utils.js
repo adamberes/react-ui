6eb55873bd0686a8a6cc0e6d315cf056c8fc66a7
Adam Beres <info@adamberes.de>
2024-06-18 13:57:22 +0200

    url changed

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://jxciooptsmxgagjgkpquk2buua0cmxrb.lambda-url.us-east-1.on.aws/tasks";
+export const API_URL = "https://jxciooptsmxgagjgkpquk2buua0cmxrb.lambda-url.us-east-1.on.aws/task";
